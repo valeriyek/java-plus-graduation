01ff52f00c82645fbd1b413a75268df51b08e341
educational1234 <agentsmith999@yandex.ru>
2025-05-22 18:32:46 +0300

    Переработать compose

--- core/event-service/src/main/resources/schema.sql
+++ core/event-service/src/main/resources/schema.sql
@@ -1,5 +1,17 @@
-DROP TABLE IF EXISTS events;
+DROP TABLE IF EXISTS event_comments, users, categories, compilations, events, compilation_events, requests;
 
+CREATE TABLE if NOT EXISTS categories
+(
+    id   BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name VARCHAR NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS users
+(
+    id    BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    email VARCHAR NOT NULL,
+    name  VARCHAR NOT NULL
+);
 
 CREATE TABLE IF NOT EXISTS events
  (
@@ -27,3 +39,45 @@ CREATE TABLE IF NOT EXISTS events
         REFERENCES categories (id)
 );
 
+CREATE TABLE IF NOT EXISTS compilations
+(
+    id     BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title  VARCHAR NOT NULL,
+    pinned BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS compilation_events
+(
+    compilation_id BIGINT NOT NULL,
+    event_id       BIGINT NOT NULL,
+    PRIMARY KEY (compilation_id, event_id),
+    FOREIGN KEY (compilation_id) REFERENCES compilations (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS requests
+(
+    id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    created      TIMESTAMP NOT NULL,
+    event_id     BIGINT    NOT NULL,
+    requester_id BIGINT    NOT NULL,
+    status       VARCHAR   NOT NULL,
+
+    CONSTRAINT fk_event FOREIGN KEY (event_id)
+        REFERENCES events (id) ON DELETE CASCADE,
+    CONSTRAINT fk_requester FOREIGN KEY (requester_id)
+        REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE TABLE event_comments
+(
+    id           BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    text         VARCHAR,
+    author_id    BIGINT,
+    event_id     BIGINT,
+    published_on TIMESTAMP WITHOUT TIME ZONE,
+    is_updated   BOOLEAN,
+    updated_on   TIMESTAMP WITHOUT TIME ZONE,
+    FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (event_id) REFERENCES events (id) ON DELETE CASCADE
+);
