2fabaebf139ccc68a441dae31ef6ec7832cd7439
educational1234 <agentsmith999@yandex.ru>
2025-05-23 02:12:56 +0300

    Доработать Event

--- core/event-service/src/main/java/ru/practicum/event/repository/EventRepository.java
+++ core/event-service/src/main/java/ru/practicum/event/repository/EventRepository.java
@@ -14,6 +14,7 @@ import java.util.Set;
 
 public interface EventRepository extends JpaRepository<Event, Long> {
 
+
     Set<Event> findByIdIn(Set<Long> ids);
 
     Page<Event> findAllByInitiator(Long userId, Pageable pageable);
